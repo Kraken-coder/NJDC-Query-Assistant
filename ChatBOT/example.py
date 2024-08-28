1d0dbd22a5dfcb4ff9887ab57aabcc9210ad9477
Kraken-coder <diptanshukumar9471@gmail.com>
2024-08-28 16:31:59 +0530

    Added requirements for the python part

--- ChatBOT/example.py
+++ ChatBOT/example.py
@@ -1,3 +1,3 @@
-from nlp import DOJVirtualAssistant
+from FinalBoT import DOJVirtualAssistant
 assistant = DOJVirtualAssistant()
 print(assistant.chatbot_response("Which state has the most number of criminal cases"))
